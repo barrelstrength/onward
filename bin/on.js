aeb3f7ffd1e45a91310ef3fc7156f1dff3b823ed
Ben Parizek <ben@barrelstrengthdesign.com>
2018-02-03 15:21:58 -0500

    Updates gulp initialization paths to be dynamic

--- bin/on.js
+++ bin/on.js
@@ -1,17 +1,35 @@
 #!/usr/bin/env node
+const path = require('path');
+const colors = require('ansi-colors');
 
-// @todo - should we rename this onward or are we safe enough calling it `on`?
+const onwardGulpDirectory = path.resolve(__dirname, '../gulpfile.js')
+const gulpModulePath = path.dirname(require.resolve('gulp'))
+const gulpBinaryFile = path.join(gulpModulePath, '/bin/gulp')
+
+// Handle any arguments
 const customArgs = require('yargs')
-  .usage('Usage: $0 [options]')
-  .command('craft', 'Setup a Craft project')
-  .describe('type', 'Type of project')
-  .example('$0 craft', 'Prepare a Craft project in the current directory')
+  .usage('Usage: yarn run $0 <gulp-task-name> [options]')
+  .describe('init', 'Setup a project with default config and template files.')
+  .example('yarn run on init', 'Copies default config and template files to your project folder:' + "\n\n" +
+    colors.grey('./gulfile.js/config/path-config.json') + "\n" +
+    colors.grey('./gulfile.js/config/task-config.js') + "\n" +
+    colors.grey('./source/fonts') + "\n" +
+    colors.grey('./source/icons') + "\n" +
+    colors.grey('./source/images') + "\n" +
+    colors.grey('./source/scripts') + "\n" +
+    colors.grey('./source/styles')
+  )
   .argv._;
 
-let args = ['--gulpfile', 'node_modules/onward/gulpfile.js'];
+// Setup the arguments we need to initialize gulp in our package directory
+// For example: ./gulp [--gulpfile onward/gulpfile.js/index.js]
+let args = ['--gulpfile', onwardGulpDirectory];
 
+// Append any additional arguments we have when we trigger gulp
+// For example: ./gulp --gulpfile onward/gulpfile.js/index.js [gulpTaskName]
 if (customArgs.length) {
   args = args.concat(customArgs);
 }
 
-require('child_process').fork('node_modules/gulp/bin/gulp', args);
+// Run onward/gulpfile.js/index.js
+require('child_process').fork(gulpBinaryFile, args);
